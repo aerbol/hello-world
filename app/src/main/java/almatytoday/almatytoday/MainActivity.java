97f47d2c3f5134ca2b9dcd344c2b156966dfe724
a_erbol <a_erbol@inbo.ru>
2016-02-01 14:16:19 +0600

    AddedProject

--- app/src/main/java/almatytoday/almatytoday/MainActivity.java
+++ app/src/main/java/almatytoday/almatytoday/MainActivity.java
@@ -4,9 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    public void button(View V){
+        TextView d =(TextView) findViewById(R.id.textView);
+        d.setText( "Almaty Today");
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
